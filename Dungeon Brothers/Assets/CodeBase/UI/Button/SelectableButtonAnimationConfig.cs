61f6d9e4ceacdee857e328486f62c11ff4ea1414
Arkty <vaitron228@mail.ru>
2023-09-23 16:07:53 +0300

    Add SelectableButton, ScaleAnimation

---
+++ Dungeon Brothers/Assets/CodeBase/UI/Button/SelectableButtonAnimationConfig.cs
@@ -0,0 +1,14 @@
+﻿using CodeBase.UI.Animations;
+using UnityEngine;
+
+namespace CodeBase.UI.Button
+{
+    [CreateAssetMenu(menuName = "StaticData/Configs/Buttons/Animation/SelectableButton", fileName = "SelectableButton")]
+    public class SelectableButtonAnimationConfig : ScriptableObject
+    {
+        [field: SerializeField] public ScaleConfig ScaleOnEntered { get; private set; }
+        [field: SerializeField] public ScaleConfig ScaleOnExited { get; private set; }
+        [field: SerializeField] public ScaleConfig ScaleOnDowned { get; private set; }
+        [field: SerializeField] public ScaleConfig ScaleOnUpped { get; private set; }
+    }
+}
