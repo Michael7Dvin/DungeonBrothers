21729bac43786527ac848e4ed00045a716c06c43
Arkty <vaitron228@mail.ru>
2023-09-23 16:07:53 +0300

    Add SelectableButton, ScaleAnimation
    (cherry picked from commit 61f6d9e4ceacdee857e328486f62c11ff4ea1414)

---
+++ Dungeon Brothers/Assets/CodeBase/UI/Button/SelectableButton.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace CodeBase.UI.Button
+{
+    public class SelectableButton : BaseButton
+    {
+        [SerializeField] private SelectableButtonAnimation selectableButtonAnimation;
+        
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            Events.PointerUpped += OnPointerUpped;
+            Events.PointerDowned += OnPointerDowned;
+            Events.PointerEntered += OnPointerEntered;
+            Events.PointerExited += OnPointerExited;
+        }
+
+        protected override void OnDisable()
+        {
+            base.OnDisable();
+            Events.PointerUpped -= OnPointerUpped;
+            Events.PointerDowned -= OnPointerDowned;
+            Events.PointerEntered -= OnPointerEntered;
+            Events.PointerExited -= OnPointerExited;
+        }
+
+        private void OnPointerUpped(PointerEventData pointerEventData)
+            => selectableButtonAnimation.ScaleOnPointerUpped();
+
+        private void OnPointerDowned(PointerEventData pointerEventData)
+            => selectableButtonAnimation.ScaleOnPointerDowned();
+
+        private void OnPointerEntered(PointerEventData pointerEventData)
+            => selectableButtonAnimation.ScaleOnPointerEntered();
+
+        private void OnPointerExited(PointerEventData pointerEventData)
+            => selectableButtonAnimation.ScaleOnPointerExited();
+    }
+}
