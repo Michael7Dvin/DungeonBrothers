f1dfe6a2eb8f4a73735cabfaa9280eeeb6a54d1c
Michael Dvin <idewishortcut@gmail.com>
2023-10-01 19:53:18 +0500

    Improve multiple tests arrange by moving creation & set up logic to Setup.cs

---
+++ src/DungeonBrothers/Assets/CodeBase/Tests/TurnQueue/TurnQueueTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using CodeBase.Gameplay.Characters;
+using CodeBase.Gameplay.Services.TurnQueue;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace CodeBase.Tests.TurnQueue
+{
+    public class TurnQueueTests
+    {
+        [Test]
+        public void WhenAddingInQueue_AndOneInitiativeIsBigger_ThenBiggerInitiativeShouldBeLast()
+        {
+            // Arrange.
+            ICharacter characterWithLessInitiative = Setup.CharacterForTurnQueue(1, 1);
+            ICharacter characterWithBiggerInitiative = Setup.CharacterForTurnQueue(1, 5);
+            
+            ITurnQueue turnQueue = Setup.TurnQueue(characterWithLessInitiative, characterWithBiggerInitiative);
+
+            // Act.
+            turnQueue.SetFirstTurn();
+
+            // Assert.
+            turnQueue.Characters.Last().Should().Be(characterWithBiggerInitiative);
+        }
+    }
+}
