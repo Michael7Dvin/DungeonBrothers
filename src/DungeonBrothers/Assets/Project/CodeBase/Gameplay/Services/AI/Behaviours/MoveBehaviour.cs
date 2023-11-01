8ebb073ccceeade63bcf096f3e72244b9b78d306
Arkty <vaitron228@mail.ru>
2023-11-01 20:31:57 +0300

    Refactor MoveBehaviour, PathFinder
    Added a new method to the pathfinder that searches by target and changed the MoveBehavior for it so that enemies can bypass each other.

--- src/DungeonBrothers/Assets/Project/CodeBase/Gameplay/Services/AI/Behaviours/MoveBehaviour.cs
+++ src/DungeonBrothers/Assets/Project/CodeBase/Gameplay/Services/AI/Behaviours/MoveBehaviour.cs
@@ -1,7 +1,7 @@
-﻿using Cysharp.Threading.Tasks;
+﻿using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
 using Project.CodeBase.Gameplay.Characters;
 using Project.CodeBase.Gameplay.Services.Map;
-using Project.CodeBase.Gameplay.Services.Move;
 using Project.CodeBase.Gameplay.Services.PathFinder;
 using Project.CodeBase.Gameplay.Tiles;
 using UnityEngine;
@@ -11,49 +11,79 @@ namespace Project.CodeBase.Gameplay.Services.AI.Behaviours
     public class MoveBehaviour : IMoveBehaviour
     {
         private readonly IPathFinder _pathFinder;
-        private readonly IMoverService _moverService;
         private readonly IMapService _mapService;
 
         private Vector2Int _currentCoordinate;
         
         public MoveBehaviour(IPathFinder pathFinder,
-            IMoverService moverService,
             IMapService mapService)
         {
             _pathFinder = pathFinder;
-            _moverService = moverService;
             _mapService = mapService;
         }
 
         public async UniTask Move(ICharacter activeCharacter,
             ICharacter target)
         {
-            _currentCoordinate = new Vector2Int(int.MaxValue, int.MaxValue);
+            List<Vector2Int> currentPath = GetCoordinatePath(target, activeCharacter);
 
-            PathFindingResults pathFindingResults = GetPathFindingResults(activeCharacter);
+            if (currentPath == null)
+                return;
 
-            Vector2Int coordinate = GetCoordinate(pathFindingResults,target);
+            Vector2Int lastCoordinate = GetCoordinate(currentPath, activeCharacter);
 
-            if (_mapService.TryGetTile(coordinate, out Tile tile)) 
-                await _moverService.Move(tile);
+            List<Tile> tilePath = GetTilePath(currentPath, lastCoordinate);
+            
+            await activeCharacter.Logic.Movement.Move(tilePath);
         }
 
-        private PathFindingResults GetPathFindingResults(ICharacter activeCharacter) =>
-            _pathFinder.CalculatePaths(activeCharacter.Logic.Movement.Coordinates,
-                activeCharacter.Logic.Movement.StartMovePoints, false);
-
-        private Vector2Int GetCoordinate(PathFindingResults pathFindingResults,
-            ICharacter target)
+        private Vector2Int GetCoordinate(List<Vector2Int> path,
+            ICharacter activeCharacter)
         {
-            foreach (var coordinate in pathFindingResults.WalkableCoordinates)
-            {
-                float distance = Vector2.Distance(coordinate, target.Logic.Movement.Coordinates);
+            int movablePoint = activeCharacter.Logic.Movement.AvailableMovePoints;
 
-                if (Vector2.Distance(_currentCoordinate, target.Logic.Movement.Coordinates) > distance) 
-                    _currentCoordinate = coordinate;
+            if (movablePoint >= path.Count)
+                return path[^2];
+            
+            return path[movablePoint - 1];
+        }
+
+        private List<Tile> GetTilePath(List<Vector2Int> path, 
+            Vector2Int lastCoordinate)
+        {
+            List<Tile> tilePath = new();
+
+            foreach (var coordinate in path)
+            {
+                if (_mapService.TryGetTile(coordinate, out Tile tile))
+                    tilePath.Add(tile);
+                    
+                if (coordinate == lastCoordinate)
+                    break;
             }
 
-            return _currentCoordinate;
+            return tilePath;
+        }
+
+        private List<Vector2Int> GetCoordinatePath(ICharacter target, 
+            ICharacter activeCharacter)
+        {
+            Vector2Int targetCoordinates = target.Logic.Movement.Coordinates;
+            
+            Vector2Int activeCharacterCoordinates = activeCharacter.Logic.Movement.Coordinates;
+            bool activeCharacterMoveThroughObstacles = activeCharacter.Logic.Movement.IsMoveThroughObstacles;
+
+            if (_mapService.TryGetTile(targetCoordinates, out Tile tile))
+            {
+                PathFindingResults pathFinding =
+                    _pathFinder.CalculatePathsByTarget(activeCharacterCoordinates, tile, activeCharacterMoveThroughObstacles);
+
+                Vector2Int tileCoordinate = tile.Logic.Coordinates;
+
+                return pathFinding.GetPathTo(tileCoordinate, false);
+            }
+
+            return null;
         }
     }
 }
--- src/DungeonBrothers/Assets/Project/CodeBase/Gameplay/Services/PathFinder/PathFinder.cs
+++ src/DungeonBrothers/Assets/Project/CodeBase/Gameplay/Services/PathFinder/PathFinder.cs
@@ -11,64 +11,127 @@ namespace Project.CodeBase.Gameplay.Services.PathFinder
         private readonly IMapService _mapService;
 
         private readonly ReactiveProperty<PathFindingResults> _pathFindingResults = new();
+
+        private Dictionary<Vector2Int, Vector2Int?> _paths;
+        private Dictionary<Vector2Int, int> _distances;
+        private Dictionary<Vector2Int, Tile> _obstacles;
+        private Queue<Vector2Int> _toVisit;
+
         public IReadOnlyReactiveProperty<PathFindingResults> PathFindingResults => _pathFindingResults;
-        
+
         public PathFinder(IMapService mapService)
         {
             _mapService = mapService;
         }
 
-        public PathFindingResults CalculatePaths(Vector2Int start, int maxDistance, bool isMoveThroughObstacles)
+        private void PrepareToStartCalculatingPaths(Vector2Int start)
         {
-            Dictionary<Vector2Int, Vector2Int?> paths = new();
-            Dictionary<Vector2Int, int> distances = new();
-            Dictionary<Vector2Int, Tile> obstacles = new();
-            Queue<Vector2Int> toVisit = new();
-            
-            toVisit.Enqueue(start);
-            distances.Add(start, 0);
-            paths.Add(start, null);
+            _paths = new();
+            _distances = new();
+            _obstacles = new();
+            _toVisit = new();
 
-            while (toVisit.Count > 0)
+            _toVisit.Enqueue(start);
+            _distances.Add(start, 0);
+            _paths.Add(start, null);
+        }
+
+        public PathFindingResults CalculatePathsByDistance(Vector2Int start, int maxDistance,
+            bool isMoveThroughObstacles)
+        {
+            PrepareToStartCalculatingPaths(start);
+
+            while (_toVisit.Count > 0)
             {
-                Vector2Int calculatingTile = toVisit.Dequeue();
+                Vector2Int calculatingTile = _toVisit.Dequeue();
 
                 foreach (Tile neighbor in _mapService.GetNeighbors(calculatingTile))
                 {
                     Vector2Int neighborCoordinates = neighbor.Logic.Coordinates;
                     bool isNeighborWalkable = neighbor.Logic.IsWalkable;
-                    int distance = distances[calculatingTile] + 1;
+                    int distance = _distances[calculatingTile] + 1;
 
                     if (isNeighborWalkable == false)
                     {
                         if (distance <= maxDistance)
-                            obstacles[neighborCoordinates] = neighbor;
+                            _obstacles[neighborCoordinates] = neighbor;
 
                         if (isMoveThroughObstacles == false)
                             continue;
                     }
 
-                    if (distance <= maxDistance)
-                    {
-                        if (paths.ContainsKey(neighborCoordinates) == false)
-                        {
-                            paths[neighborCoordinates] = calculatingTile;
-                            distances[neighborCoordinates] = distance;
-                            toVisit.Enqueue(neighborCoordinates);
-                        }
-                        else if (distances[neighborCoordinates] > distance)
-                        {
-                            paths[neighborCoordinates] = calculatingTile;
-                            distances[neighborCoordinates] = distance;
-                        }
-                    }
+                    if (distance <= maxDistance) 
+                        AddTileToPath(neighborCoordinates, calculatingTile, distance);
                 }
             }
 
-            obstacles.Remove(start);
-            return new PathFindingResults(paths, obstacles);
+            _obstacles.Remove(start);
+            return new PathFindingResults(_paths, _obstacles);
         }
 
+        public PathFindingResults CalculatePathsByTarget(Vector2Int start, Tile tile, bool isMoveThroughObstacles)
+        {
+            PrepareToStartCalculatingPaths(start);
+
+            while (_toVisit.Count > 0)
+            {
+                Vector2Int calculatingTile = _toVisit.Dequeue();
+
+                foreach (Tile neighbor in _mapService.GetNeighbors(calculatingTile))
+                {
+                    Vector2Int neighborCoordinates = neighbor.Logic.Coordinates;
+                    bool isNeighborWalkable = neighbor.Logic.IsWalkable;
+                    int distance = _distances[calculatingTile] + 1;
+
+                    if (neighbor == tile)
+                    {
+                        _toVisit.Clear();
+                        _paths[neighborCoordinates] = calculatingTile;
+                        break;
+                    }
+
+                    if (isNeighborWalkable == false)
+                    {
+                        _obstacles[neighborCoordinates] = neighbor;
+
+                        if (isMoveThroughObstacles == false)
+                            continue;
+                    }
+
+                    AddTileToPath(neighborCoordinates, calculatingTile, distance);
+                }
+            }
+
+            _obstacles.Remove(start);
+            return new PathFindingResults(_paths, _obstacles);
+        }
+
+        private void AddTileToPath(Vector2Int neighborCoordinates, Vector2Int calculatingTile, int distance)
+        {
+            if (_paths.ContainsKey(neighborCoordinates) == false)
+            {
+                _paths[neighborCoordinates] = calculatingTile;
+                _distances[neighborCoordinates] = distance;
+                _toVisit.Enqueue(neighborCoordinates);
+            }
+            else if (_distances[neighborCoordinates] > distance)
+            {
+                _paths[neighborCoordinates] = calculatingTile;
+                _distances[neighborCoordinates] = distance;
+            }
+        }
+
+        private bool TryContinueCalculatingPath(Vector2Int neighborCoordinates)
+        {
+            if (_paths.ContainsKey(neighborCoordinates) == false)
+            {
+                _toVisit.Enqueue(neighborCoordinates);
+                return true;
+            }
+
+            return false;
+        }
+        
         public void SetPathFindingResults(PathFindingResults pathFindingResults) =>
             _pathFindingResults.SetValueAndForceNotify(pathFindingResults);
     }
